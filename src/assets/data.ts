c0740407bcd1c71acfd03d41f6d4ab27b2dc2cc4
Jessica-Koch <jessicakoch136@icloud.com>
2024-04-05 21:41:56 -0700

    add base configuration and app

---
+++ src/assets/data.ts
@@ -0,0 +1,61 @@
+import { Question } from "./types";
+
+export const QUESTION_BANK: Question[] = [
+  {
+    question: "What piece on a chess board moves in an L-shape?",
+    options: [
+      'the knight', 'the bishop', 'the queen', 'the king'
+    ],
+    correctAnswer: 0
+  },
+   {
+    question: "How many sides does a stop sign have?",
+    options: [
+      '5', '6', '7', '8'
+    ],
+    correctAnswer: 3
+  
+  },
+   {
+    question: "What is the pH of pure water?",
+    options: [
+      '5', '1', '7', '10.25'
+    ],
+    correctAnswer: 2
+  },
+   {
+    question: "What is a telomere?",
+    options: [
+      'A method of magnification', 'A region of repetitive DNA which protects teh end of a chromosome', 'A tear in spacetime, commonly known as a worm-hole', 'Perturbations in the Earth\'s orbit'
+    ],
+    correctAnswer: 1
+  },
+   {
+    question: "Which of these is NOT a type of architecture?",
+    options: [
+      'Art Deco', 'Brutalist', 'Dadaist', 'Gothic'
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Which of these musical instruments is most similar to a fife?",
+    options: [
+      'Flute', 'Violin', 'Trumpet', 'Banjo'
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which country has the most African elephants of any nation in the world?",
+    options: [
+      'Botswana', 'Kenya', 'South Africa', 'Nigeria'
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "Which is the longest interstate in the United States?",
+    options: [
+      'I-40', 'I-80', 'I-10', 'I-90'
+    ],
+    correctAnswer: 3
+  },
+]
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+// import { Countdown } from '../src2/components/Countdown/Countdown';
+// import { Quiz } from '../src2/components/Quiz/Quiz';
+import './App.scss';
+import { Quiz } from './Quiz/Quiz';
+
+function App() {
+  return (
+    <div className='app'>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
